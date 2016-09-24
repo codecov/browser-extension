78cd7725372ce4199f7df0e53b83e7045da23be2
Steve Peak <steve@stevepeak.net>
2016-09-24 07:33:38 -0400

    fix pjax event, close https://github.com/codecov/support/issues/295

--- src/firefox/firefox.js
+++ src/firefox/firefox.js
@@ -9,7 +9,7 @@ var storage_set = function(key, value, cb){
 
 self.port.on("preferences", function(prefs){
   window.codecov = create_codecov_instance(prefs);
-  window.addEventListener("message", function(event){
+  window.addEventListener("pjax:success", function(event){
     // cannot figure out how to attach to pjax:success, but this is a hack
     window.codecov._start();
   }, false);
