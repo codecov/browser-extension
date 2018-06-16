5d8f8eff3c1f6d56aca780e4466fa02f08e91b10
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-06-16 09:49:49 +0200

    Removed jQuery usage in listener, fixes #55

--- src/chrome/listener.js
+++ src/chrome/listener.js
@@ -1,11 +1,3 @@
-if ($ == null || $ == undefined) {
-  document.addEventListener("pjax:success", function(){
-    window.postMessage({type:"codecov"},"*");
-  });
-} else {
-  $(function(){
-    $(document).on('pjax:success', function(){
-      window.postMessage({type:"codecov"},"*");
-    });
-  });
-}
+document.addEventListener("pjax:success", function(){
+  window.postMessage({type:"codecov"},"*");
+});
